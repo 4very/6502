55dfd08ae222fccacd765471ac33e07526e7c975
4very <asommer999@gmail.com>
2022-03-05 19:30:30 -0500

    added minimal working emulator example

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./compilier/bin/vasm/v1.9/vasm6502_oldstyle -Fbin ./programs/src/$1.s -o ./programs/bin/$1.bin
--- emulator/start.py
+++ emulator/start.py
@@ -1,11 +1,34 @@
 
+# from https://github.com/docmarionum1/py65emu/blob/master/README.rst
+
 from lib.py65emu.py65emu.cpu import CPU
 from lib.py65emu.py65emu.mmu import MMU
 
 
+filename = 'start'
+# lda #$ff
+# sta $0x00
 
+f = open(r'./programs/bin/' + filename + '.bin', 'rb')
 
-m = MMU([
+mmu = MMU([
     (0x00, 0x200),
     (0x1000, 0x4000, True, f)
 ])
+
+c = CPU(mmu, 0x1000)
+
+c.step()
+c.step()
+
+
+print(c.r.a)    # A register
+print(c.r.x)    # X register
+print(c.r.y)    # Y register
+print(c.r.s)    # Stack Pointer
+print(c.r.pc)   # Program Counter
+
+print(c.cc)
+
+print(c.r.getFlag('C'))
+print(mmu.read(0x00))
